a4778447deb6e58a8f7b80d16a978e7ca1589869
Alex Scott <alexander.scott@utsouthwestern.edu>
2024-06-12 14:14:00 -0500

    Updated the redability of playbook.sh output.

--- playbook.sh
+++ playbook.sh
@@ -7,19 +7,27 @@ function list_playbooks {
 	#COMMANDS=$(find $DIR/scripts -type f | grep "\.command\.sh$" | sed "s|.*/\([a-zA-Z_-]*\)\.command\.sh$|\1|g" | sort)
 	local playbooks=$(find $DIR/playbooks -name "*.yaml" -type f | sed 's/\.\/playbooks\///g;s/.yaml//g' | sort)
 	echo "Available playbooks:"
+	echo ""
 	for playbook in $playbooks; do
-		echo "$playbook"
+		echo "	$playbook"
 	done
+	echo ""
 }
 
-PLAYBOOK="playbooks/$1.yaml"
+PLAYBOOK=$1
 shift
+PLAYBOOK_FILE="playbooks/$PLAYBOOK.yaml"
 
-if [ ! -f "$PLAYBOOK" ]; then
+if [ -z "$PLAYBOOK" ]; then
 	echo "No playbook specified!"
 	list_playbooks
 	exit
 fi
 
-ansible-playbook -i ./inventory.yaml -e @vars.yaml --vault-password-file ./ansible_encryption_password $PLAYBOOK $@
+if [ ! -f "$PLAYBOOK_FILE" ]; then
+	echo "Playbook '$PLAYBOOK' not found!"
+	list_playbooks
+	exit
+fi
 
+ansible-playbook -i ./inventory.yaml -e @vars.yaml --vault-password-file ./ansible_encryption_password $PLAYBOOK_FILE $@
